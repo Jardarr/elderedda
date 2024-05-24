8ba98fc4be1f1eb27d85d8c7303b93a70eb10546
Jardarr <138442652+Jardarr@users.noreply.github.com>
2024-05-24 10:53:42 +0300

    fix: lang ru

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <body className={inter.className}>
         <div>
           <div className="px-4 md:px-24">
