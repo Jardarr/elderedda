7386c4cbce92554606ae50c1a73fececc03aa23e
Jardarr <dmitrynordmann@gmail.com>
2024-06-15 13:43:04 +0300

    fix: color link in footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,25 +2,25 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="flex flex-col lg:justify-between lg:flex-row md:text-sm text-gray-500 text-xs">
+    <div className="flex flex-col lg:justify-between lg:flex-row md:text-xs text-gray-500 text-xs">
       <div className="flex justify-center flex-col">
         <div className="flex justify-center items-center flex-wrap">
-          <Link className="px-2" href="/poems/about-gods">
+          <Link className="px-2 hover:text-gray-300" href="/poems/about-gods">
             Goðakvæði
           </Link>
-          <Link className="px-2" href="/poems/about-heroes">
+          <Link className="px-2 hover:text-gray-300" href="/poems/about-heroes">
             Hetjukvæði
           </Link>
-          <Link className="px-2" href="/poems/edda-songs">
+          <Link className="px-2 hover:text-gray-300" href="/poems/edda-songs">
             Eddukvæði
           </Link>
-          <Link className="px-2" href="/poems/edda-app">
+          <Link className="px-2 hover:text-gray-300" href="/poems/edda-app">
             Viðbætir
           </Link>
-          <Link className="px-2" href="/poems/poetry">
+          <Link className="px-2 hover:text-gray-300" href="/poems/poetry">
             Ljóð
           </Link>
-          <Link className="px-2" href="/poems/stora-edda">
+          <Link className="px-2 hover:text-gray-300" href="/poems/stora-edda">
             Stóra Edda
           </Link>
         </div>
