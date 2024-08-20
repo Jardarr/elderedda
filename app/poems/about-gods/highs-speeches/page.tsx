369545127dffebe254fab887ca323af2e74dd9a5
Jardarr <dmitrynordmann@gmail.com>
2024-08-20 21:20:50 +0300

    fix: right formatted

--- app/poems/about-gods/highs-speeches/page.tsx
+++ app/poems/about-gods/highs-speeches/page.tsx
@@ -1,52 +1,52 @@
 import React from "react";
 import { highsspeeches } from "./page.utils";
-import { Metadata } from 'next'
- 
+import { Metadata } from "next";
+
 export const metadata: Metadata = {
-  title: 'Jardarr | Hávamál',
-  description: 'Старшая эдда - Речи Высокого',
-  keywords: 'Старшая Эдда, поэзия, древний обычай',
-  viewport: 'width=device-width, initial-scale=1.0',
-  openGraph: {
-    title: 'Jardarr | Hávamál',
-    description: 'Старшая эдда - Речи Высокого',
-    siteName: 'Jardarr | Elder Edda',
-    images: [
-      {
-        url: '/og-logo.jpg',
-        width: 800,
-        height: 600,
-      },
-    ],
-    locale: 'ru_RU',
-    type: 'website',
-  },
-}
+    title: "Jardarr | Hávamál",
+    description: "Старшая эдда - Речи Высокого",
+    keywords: "Старшая Эдда, поэзия, древний обычай",
+    viewport: "width=device-width, initial-scale=1.0",
+    openGraph: {
+        title: "Jardarr | Hávamál",
+        description: "Старшая эдда - Речи Высокого",
+        siteName: "Jardarr | Elder Edda",
+        images: [
+            {
+                url: "/og-logo.jpg",
+                width: 800,
+                height: 600,
+            },
+        ],
+        locale: "ru_RU",
+        type: "website",
+    },
+};
 export default function HighsSpeeches() {
-	return (
-		<main className="flex items-center justify-center bg-poem-bg bg-cover bg-fixed bg-top bg-no-repeat text-sm md:text-base">
-			<div className="flex flex-col w-[600px] bg-gray-800/70 rounded-md">
-				<div className="m-8 text-3xl text-teal-800 text-center font-bold">
-					<h1>Hávamál</h1>
-					<h2>Речи Высокого</h2>
-				</div>
-				{highsspeeches.map((poem, id) => {
-					return (
-						<div key={id} className={poem.class}>
-							<div>{poem.number}</div>
-							<div>{poem.contentON}</div>
-							<div>{poem.number}</div>
-							<div>{poem.contentRU}</div>
-						</div>
-					);
-				})}
-				<div className="flex justify-center my-8 mx-4">
-					<span>
-						<p>Древнеисландский текст, ред. Guðni Jónsson</p>
-						<p>Текст на русском языке в переводе А. И. Корсуна</p>
-					</span>
-				</div>
-			</div>
-		</main>
-	);
+    return (
+        <main className="flex items-center justify-center bg-poem-bg bg-cover bg-fixed bg-top bg-no-repeat text-sm md:text-base">
+            <div className="flex flex-col w-[600px] bg-gray-800/70 rounded-md">
+                <div className="m-8 text-3xl text-teal-800 text-center font-bold">
+                    <h1>Hávamál</h1>
+                    <h2>Речи Высокого</h2>
+                </div>
+                {highsspeeches.map((poem, id) => {
+                    return (
+                        <div key={id} className={poem.class}>
+                            <div>{poem.number}</div>
+                            <div>{poem.contentON}</div>
+                            <div>{poem.number}</div>
+                            <div>{poem.contentRU}</div>
+                        </div>
+                    );
+                })}
+                <div className="flex justify-center my-8 mx-4">
+                    <span>
+                        <p>Древнеисландский текст, ред. Guðni Jónsson</p>
+                        <p>Текст на русском языке в переводе А. И. Корсуна</p>
+                    </span>
+                </div>
+            </div>
+        </main>
+    );
 }
