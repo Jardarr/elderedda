6570236d2d724477f65cbba3404c0848b34c920f
Jardarr <dmitrynordmann@gmail.com>
2025-01-29 21:56:17 +0300

    fix: spinner loader

--- app/components/Spinner.tsx
+++ app/components/Spinner.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 export default function Spinner() {
     return (
         <div className="flex flex-col items-center justify-center h-24">
-            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900">
-                <Image className="rotate-360" src="/spinner.png" alt="Loading..." width={100} height={100} />
+            <div className="animate-spin rounded-full border-t-2 border-b-2 border-gray-900">
+                <svg xmlns="http://www.w3.org/2000/svg" width="54" height="54" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-loader-pinwheel rotate-360"><path d="M22 12a1 1 0 0 1-10 0 1 1 0 0 0-10 0"/><path d="M7 20.7a1 1 0 1 1 5-8.7 1 1 0 1 0 5-8.6"/><path d="M7 3.3a1 1 0 1 1 5 8.6 1 1 0 1 0 5 8.6"/><circle cx="12" cy="12" r="10"/></svg>
             </div>
         </div>
     );
